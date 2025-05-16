837a268afa40af085b59284e9294abd379bcc5c9
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-05-16 07:53:52 +0530

    adding tests

---
+++ src/test/java/com/testFramework/tests/crud/TestHealthCheck.java
@@ -0,0 +1,26 @@
+package com.testFramework.tests.crud;
+
+
+import com.testFramework.base.BaseTest;
+import com.testFramework.endpoints.APIConstants;
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.requestSpecification;
+
+public class TestHealthCheck extends BaseTest {
+    @Test
+    @Description("TC#3  - Verify Health")
+    public void testGETHealthCheck() {
+        requestSpecification.basePath(APIConstants.PING_URL);
+
+        response = RestAssured.given(requestSpecification).when().get();
+
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(201);
+
+
+    }
+}
---
+++ src/test/java/com/testFramework/tests/e2e_integration/TestIntegrationFlow2.java
@@ -0,0 +1,31 @@
+package com.testFramework.tests.e2e_integration;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+public class TestIntegrationFlow2 {
+
+    @Test(groups = "qa", priority = 1)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 1. Verify that the Booking can be Created")
+    public void testCreateBooking(ITestContext iTestContext) {
+
+    }
+
+    @Test(groups = "qa", priority = 4)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 2. Delete the Booking by ID")
+    public void testDeleteBookingById(ITestContext iTestContext) {
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+    }
+
+    @Test(groups = "qa", priority = 2)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 3. Verify that the Booking By ID")
+    public void testVerifyBookingId(ITestContext iTestContext) {
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+
+    }
+}
